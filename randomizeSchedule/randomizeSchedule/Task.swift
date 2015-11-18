196fa150d6b747b6986ff9d0769e4e681b529814
Thang Tong <tongthang0104@gmail.com>
2015-11-18 14:22:39 -0700

    ViewHierachy Set up

---
+++ randomizeSchedule/randomizeSchedule/Task.swift
@@ -0,0 +1,22 @@
+//
+//  Task.swift
+//  randomizeSchedule
+//
+//  Created by Thang H Tong on 11/18/15.
+//  Copyright © 2015 Thang. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name: String
+    var timeToDo: String
+    
+    init(name: String, timeToDo: String) {
+        self.name = name
+        self.timeToDo = timeToDo
+    }
+}
+
+
+
