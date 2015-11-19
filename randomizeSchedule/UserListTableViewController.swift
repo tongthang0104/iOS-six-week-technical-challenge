6046b8489a461d19aa7bfc253015438355aa6588
Thang Tong <tongthang0104@gmail.com>
2015-11-19 02:27:27 -0700

    UserListDelete func implemented

--- randomizeSchedule/UserListTableViewController.swift
+++ randomizeSchedule/UserListTableViewController.swift
@@ -54,7 +54,7 @@ class UserListTableViewController: UITableViewController {
             lastNameField.placeholder = "LastName"
             
         }
-    
+        
         presentViewController(alert, animated: true, completion: nil)
         
     }
@@ -89,17 +89,17 @@ class UserListTableViewController: UITableViewController {
     }
     */
     
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-    // Delete the row from the data source
-    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            let people = PeopleController.shareController.peopleArray[indexPath.row]
+            PeopleController.shareController.remove(people)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
     }
-    }
-    */
+    
     
     /*
     // Override to support rearranging the table view.
